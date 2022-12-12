1fe0b73a4ea521b2e30e6d61ede7de662a327b67
honghanh426417 <114908283+honghanh426417@users.noreply.github.com>
2022-12-12 15:48:13 +0700

    add

--- CRUD JSON file AJS/data.js
+++ CRUD JSON file AJS/data.js
@@ -3,7 +3,21 @@ var app = angular.module("App", []);
 app.controller('ContactController', function ($scope) {
     var uid = 1;
     $scope.contacts = [
-        { id: 0, 'name' : 'Ngo Anh Tuan', 'email': 'tuananhngo@gmail.com', 'phone': '111-222-333'}
+        { id: 0, 'name' : 'Ngo Anh Tuan', 'email': 'tuananhngo@gmail.com', 'phone': '111-222-333'
+          
+    },
+    { id: 1, 'name' : 'Ngo Thanh Thao', 'email': 'thanhthao@gmail.com', 'phone': '123445'
+          
+    },
+    { id: 2, 'name' : 'Nguyen Duc Tho', 'email': 'duc tho@gmail.com', 'phone': '4578349'
+          
+    },
+    { id: 3, 'name' : 'Do Thi Hong Hanh', 'email': 'honghanh@gmail.com', 'phone': '534739'
+          
+    },
+    { id: 4, 'name' : 'Van Toan', 'email': 'vantoan@gmail.com', 'phone': '0987656'
+          
+    },
     ];
 
     $scope.saveContact = function () {
